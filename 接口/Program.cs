307ff59f55ccb8520655580d7aff4241f8649dd3
Moyufan <2568495415@qq.com>
2018-12-29 10:17:50 +0800

    C#学习

--- 接口/Program.cs
+++ 接口/Program.cs
@@ -6,7 +6,75 @@ namespace 接口
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //虚方法，抽象类，接口，实现多态的三种方法
+            NBAplayer nba = new Student();
+            nba.KouLan();
+            Superman fly = new Teacher();
+            fly.Fly();
+            Console.ReadKey();
+           
+        }
+
+    }
+
+    public class Person
+    {
+        public void eat()
+        {
+            Console.WriteLine("人会吃");
         }
     }
+
+    //一个子类只允许一个父类，但是可以继承多个接口
+    //子类继承接口后，必须实现接口中的所有方法
+
+    public class Student : Person, NBAplayer, Superman
+    {
+        public void KouLan()
+        {
+            Console.WriteLine("学生可以扣烂了");
+        }
+
+        void Superman.Fly()
+        {
+            Console.WriteLine("学生可以飞了");
+        }
+    }
+
+    public class Teacher : NBAplayer, Superman
+    {
+        public void Fly()
+        {
+            Console.WriteLine("老师可以飞了");
+        }
+
+        public void KouLan()
+        {
+            Console.WriteLine("老师可以扣烂了");
+        }
+    }
+    public interface NBAplayer
+    {
+        void KouLan();     
+               
+    }
+
+    public interface Superman
+    {
+        void Fly();
+    }
+    //public class NBAplaer 
+    //{
+    //    public void Koulan()
+    //    {
+    //        Console.WriteLine("NBA球员可以扣篮！");
+    //    }
+    //}
+    //public class SuperMan 
+    //{
+    //    public void Fly()
+    //    {
+    //        Console.WriteLine("超人飞、、、、、");
+    //    }
+    //}
 }
