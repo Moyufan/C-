3c66944f6ad19ff7c44e78ac559a8598167284d7
Moyufan <2568495415@qq.com>
2018-12-29 11:47:14 +0800

    interface

--- 接口/Program.cs
+++ 接口/Program.cs
@@ -27,7 +27,9 @@ namespace 接口
 
     //一个子类只允许一个父类，但是可以继承多个接口
     //子类继承接口后，必须实现接口中的所有方法
-
+    //接口，抽象类不可以创建对象
+    //接口中只允许出现方法，默认public修饰，不能包含字段
+    //接口与接口之间可以继承，多继承，不可以继承与类
     public class Student : Person, NBAplayer, Superman
     {
         public void KouLan()
---
+++ 显示实现接口/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace 显示实现接口
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MQBird maque = new MQBird();
+            maque.Fly();
+            QQBird qq = new QQBird();
+            qq.Fly();
+            qq.Eat();
+            Ifly qwq = new QQBird();
+            qwq.Fly();
+            
+            Console.ReadKey();
+
+        }
+    }
+    //接口像是一种功能，给有此能力的对象继承
+    public class MQBird:Ifly
+    {
+        public void Fly()
+        {
+            Console.WriteLine("麻雀飞呀飞。。。");
+        }
+    }
+    public class QQBird:Ifly
+    {
+        public void Fly()
+        {
+            Console.WriteLine("企鹅也会飞。");
+
+        }
+        void Ifly.Fly()
+        {
+            Console.WriteLine("企鹅用脚飞哦O(∩_∩)O哈哈~");
+        }
+        public void  Eat()
+        {
+            Console.WriteLine("企鹅爱吃。。。");
+        }
+    }
+
+    public interface Ifly
+    {
+        void Fly();
+    }
+}
